a59d8272789f9a95778638e248a096d5f64516d1
oliverhugh <oliver.hugh.1073@gmail.com>
2025-03-20 11:06:59 -0400

    Reworked trajectory planning of puck

--- kinematics.py
+++ kinematics.py
@@ -2,7 +2,7 @@
 # assumes coordinates align, will need a new function later for 
 
 # we should use an average of these to get a better path
-def trajectory_prediction(center1, time1, center2, time2):
+def trajectory_prediction(center1, time1, center2, time2, x_end, y_end, goal_start=20, goal_end=80):
     """
 
     @param center1: (x, y) coordinates at time1
@@ -20,15 +20,35 @@ def trajectory_prediction(center1, time1, center2, time2):
     y_table_edge_max = 100
     y_table_edge_min = 0
 
+    x1,y1 = center1
+    x2, y2 = center2
+
+
     # Placeholder, will likely need to change based on format of time1 and time2
     delta_t = time2 - time1
 
     # Calculate time derivatives of x and y, assuming frictionless surface
-    v_x = (center2[0] - center1[0]) / 2
-    v_y = (center2[1] - center1[1]) / 2
+    v_x = (x2 - x1) / delta_t
+    v_y = (y2 - y1) / delta_t
 
     # Solve for slope and y-intercept
     m = v_y / v_x
-    b = m * -1 * center2[0] + center2[1]
+    b = m * -1 * x2 + y2
+
+    y_predict = m*x_end +b
+
+
+
+    while v_x > 0:
+        if goal_start <= y_predict <= goal_end:
+            #move robot to y_intercept
+            True  
+        else:
+            x_collision = (y_end-b)/m
+            y_new = -m * ( x_end+ (b-y_end))+ (x_collision +b)
+
+
+        
 
     return m, b
+
